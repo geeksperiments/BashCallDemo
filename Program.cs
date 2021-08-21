d25b12823bcc587864d47b038c9f8c5cb37f6907
Paul Amazona <whatevergeek@gmail.com>
2021-08-21 21:46:44 +0800

    initial files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BashCallDemoHelpers;
+using log4net;
+
+namespace BashCallDemo
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executing bash...");
+            "touch test.txt".Bash(log);
+        }
+    }
+}
---
+++ ShellHelper.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using log4net;
+
+namespace BashCallDemoHelpers
+{
+    public static class ShellHelper
+    {
+        public static Task<int> Bash(this string cmd, ILog log)
+        {
+            var source = new TaskCompletionSource<int>();
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                },
+                EnableRaisingEvents = true
+            };
+            process.Exited += (sender, args) =>
+              {
+                  log.Warn(process.StandardError.ReadToEnd());
+                  log.Info(process.StandardOutput.ReadToEnd());
+                  if (process.ExitCode == 0)
+                  {
+                      source.SetResult(0);
+                  }
+                  else
+                  {
+                      source.SetException(new Exception($"Command `{cmd}` failed with exit code `{process.ExitCode}`"));
+                  }
+
+                  process.Dispose();
+              };
+
+            try
+            {
+                process.Start();
+            }
+            catch (Exception e)
+            {
+                log.Error($"Command {cmd} failed");
+                source.SetException(e);
+            }
+
+            return source.Task;
+        }
+    }
+}
